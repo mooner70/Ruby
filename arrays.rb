97aeed57f6de9d944fdcf26937fab2fc6b3324f3
Andy Moon <mooner@andy-moon.com>
2018-05-28 17:39:32 -0500

    adding Arrays assignement

---
+++ arrays.rb
@@ -0,0 +1,15 @@
+a = [ 'Do', 'or', 'Do', 'not,', 'there', 'is', 'no', 'try.' ]
+
+# puts a.at(2)
+# puts a.fetch(2)
+# puts a.delete_at(4)
+# puts a
+# puts a.reverse
+# puts a.length
+# puts a.sort
+# puts a.slice(4)
+# puts a.shuffle
+# puts a.join(" ")
+# puts a.insert(8, "~Yoda").join(" ")
+puts a.values_at(0,1,2,3).join(" ")
+
