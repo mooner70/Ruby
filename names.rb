fef3849cbea82b839f36871ead0418985b0b4912
Andy Moon <mooner@andy-moon.com>
2018-05-29 15:59:00 -0500

    added Names assignment

---
+++ names.rb
@@ -0,0 +1,13 @@
+def names
+  a = {first_name: "Michael", last_name: "Choi"}
+  b = {first_name: "John", last_name: "Doe"}
+  c = {first_name: "Jane", last_name: "Doe"}
+  d = {first_name: "James", last_name: "Smith"}
+  e = {first_name: "Jennifer", last_name: "Smith"}
+  names = [a, b, c, d, e]
+  puts "You have #{names.length} names in the 'names' array."
+  puts "You have #{names.size} names in the 'names' array."
+  puts "You have #{names.count} names in the 'names' array."
+  names.each { |name| puts "The name is '#{name[:first_name]} #{name[:last_name]}'"}
+  end
+names
